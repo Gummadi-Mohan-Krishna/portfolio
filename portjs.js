9e0c1daf76d55b6f2157b8e2e452522956bfd1e9
Gummadi-Mohan-Krishna <gmohankrishnawork@gmail.com>
2025-02-27 12:47:57 +0530

    Add files via upload

---
+++ portjs.js
@@ -0,0 +1,15 @@
+document.getElementById("about").addEventListener("click",()=>{
+    window.location.href="about.html";
+});
+
+document.getElementById("skill").addEventListener("click",()=>{
+    window.location.href="skills.html";
+});
+
+document.getElementById("project").addEventListener("click",()=>{
+    window.location.href="project.html";
+});
+
+document.getElementById("contact").addEventListener("click",()=>{
+    window.location.href="contact.html";
+});
